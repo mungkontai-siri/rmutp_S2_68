329f083b78c88bec775c719c74931006db65b928
mungkontai-siri <mungkontai-s@rmutp.ac.th>
2025-09-13 23:46:23 +0700

    add more scripts with dev

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app = new Hono();
 // operation
 // CRUD // 
 
-app.get("/", (c) => c.text("Hello World"));
+app.get("/", (c) => c.text("Hello World Today!"));
 app.get("/profile", (c) => c.text("Profile"));
 
 export default app;
