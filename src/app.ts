8a531566415563b6802f681c721c756d53899bfa
mungkontai-siri <mungkontai-s@rmutp.ac.th>
2025-09-14 01:46:53 +0700

    using prima client call to db by profile

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = new Hono();
 
@@ -6,6 +9,14 @@ const app = new Hono();
 // CRUD // 
 
 app.get("/", (c) => c.text("Hello World Today!"));
-app.get("/profile", (c) => c.text("Profile"));
+app.get("/profile", async (c) => {
+    //get data from db
+    const profiles = await prisma.profile.findMany();
+    //response
+    return c.json({
+        message: "get data completed",
+        data: profiles
+    }, 200);
+});
 
 export default app;
